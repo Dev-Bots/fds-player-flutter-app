e31e14ac5d91de835d46c8039d9b6b52cb1bd94b
Tinsae <itistinsae@gmail.com>
2022-03-25 14:57:51 +0300

    Update Account.dart

--- lib/Data/models/Account.dart
+++ lib/Data/models/Account.dart
@@ -1,1 +1,22 @@
+// ignore_for_file: file_names
+import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
 
+// import 'package:mobile_banking/domain/models/Event.dart';
+
+@immutable
+class Account extends Equatable {
+  const Account({
+    this.accountID,
+    required this.firstName,
+    required this.lastName,
+    this.fullName,
+    required this.userName,
+    required this.gender,
+    // this.profilePicture,
+    required this.dob,
+    required this.email,
+    required this.phoneNumber,
+    required this.address,
+    // this.registeredUsers,
+  });
