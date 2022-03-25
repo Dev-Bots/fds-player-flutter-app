d847af4f0a99ea8947bb7054300d0cc4eef90dfe
Tinsae <itistinsae@gmail.com>
2022-03-25 13:31:03 +0300

    Update account_bloc.dart

--- lib/Bloc/AccountBloc/account_bloc.dart
+++ lib/Bloc/AccountBloc/account_bloc.dart
@@ -1,1 +1,32 @@
+import 'package:bloc/bloc.dart';
+import 'package:dev_player_fds/Data/repository/repository.dart';
+import 'package:equatable/equatable.dart';
 
+part 'account_event.dart';
+part 'account_state.dart';
+
+class AccountBloc extends Bloc<AccountEvent, AccountState> {
+  final AccountRepository _accountRepository;
+
+  AccountBloc(this._accountRepository) : super(AccountLoading()) {
+    on<GetMyAccount>((event, emit) async {
+      print('event dispatched');
+      var user = await _accountRepository.getAllAccounts();
+      print('returned here');
+      if (user != null) {
+        print('user is not null');
+        emit(AccountLoaded(user: user));
+      } else {
+        emit(const AccountFailed(error: "Failed to load."));
+      }
+    });
+    on<GetOtherAccounts>((event, emit) async {
+      var user = await _accountRepository.getCurrentUser();
+      if (user != null) {
+        emit(AccountLoaded(user: user));
+      } else {
+        emit(const AccountFailed(error: "Failed to load."));
+      }
+    });
+  }
+}
