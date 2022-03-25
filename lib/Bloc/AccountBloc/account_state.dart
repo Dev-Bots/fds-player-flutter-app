1e3cd67154943546af0549310731f82706b69b9d
Tinsae <itistinsae@gmail.com>
2022-03-25 13:52:39 +0300

    Update account_state.dart

--- lib/Bloc/AccountBloc/account_state.dart
+++ lib/Bloc/AccountBloc/account_state.dart
@@ -1,1 +1,21 @@
+part of 'account_bloc.dart';
+
+abstract class AccountState extends Equatable {
+  const AccountState();
+
+  @override
+  List<Object> get props => [];
+}
+
+class AccountInitial extends AccountState {}
+
+class AccountLoading extends AccountState {}
+
+class AccountLoaded extends AccountState {
+  final user;
+
+  const AccountLoaded({required this.user});
+  @override
+  List<Object> get props => [user];
+}
 
