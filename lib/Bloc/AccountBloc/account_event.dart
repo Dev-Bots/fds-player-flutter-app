290f58a22134f7a8f8d6e1ae57c48fdbbdd00b1c
Tinsae <itistinsae@gmail.com>
2022-03-25 13:44:50 +0300

    Update account_event.dart

--- lib/Bloc/AccountBloc/account_event.dart
+++ lib/Bloc/AccountBloc/account_event.dart
@@ -1,1 +1,18 @@
+part of 'account_bloc.dart';
 
+abstract class AccountEvent extends Equatable {
+  const AccountEvent();
+
+  @override
+  List<Object> get props => [];
+}
+
+class GetMyAccount extends AccountEvent {
+  @override
+  List<Object> get props => [];
+}
+
+class GetOtherAccounts extends AccountEvent {
+  @override
+  List<Object> get props => [];
+}
