f18dfb313d04b2668db069c377907f25119893fa
Tinsae <itistinsae@gmail.com>
2022-03-30 00:35:06 +0300

    Update BackButton.dart

--- lib/Presentation/widgets/BackButton.dart
+++ lib/Presentation/widgets/BackButton.dart
@@ -1,1 +1,19 @@
 
+import 'package:flutter/material.dart';
+
+class BackButton extends StatelessWidget {
+  const BackButton({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+        padding: EdgeInsets.only(left: 20),
+        onPressed: () {},
+        icon: Icon(
+          Icons.arrow_back_ios,
+          color: Colors.grey.shade600,
+        ));
+  }
+}
