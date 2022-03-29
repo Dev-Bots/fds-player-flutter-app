d2c0c37b78b6331acba4dadd31e05739e7a4bd75
Tinsae <itistinsae@gmail.com>
2022-03-29 19:53:38 +0300

    Update routes.dart

--- lib/Presentation/config/routes.dart
+++ lib/Presentation/config/routes.dart
@@ -20,3 +20,28 @@ class RouteGenerator {
     }
   }
 }
+// If we need to pass argument to the next screen, we can use the below
+// We can create a Route<dynamic> classed error handler like in Flutter Routes & Navigation – Parameters, Named Routes, onGenerateRoute video on Reso Coder
+
+
+  // static Route<dynamic> generateRouteWithArgs(RouteSettings settings) {
+  //   switch (settings.name) {
+  //     case loginPage:
+  //       return MaterialPageRoute(
+  //           builder: (_) => const LoginPage(
+  //                 user: settings.arguments,
+  //               ));
+  //     case homePage:
+  //       return MaterialPageRoute(
+  //           builder: (_) => const HomePage(
+  //                 user: settings.arguments,
+  //               ));
+  //     case registerPage:
+  //       return MaterialPageRoute(
+  //           builder: (_) => const RegisterPage(
+  //                 user: settings.arguments,
+  //               ));
+  //     default:
+  //       throw const FormatException("Route was not found");
+  //   }
+  // }
