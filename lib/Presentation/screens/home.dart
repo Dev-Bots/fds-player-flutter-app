803d56dad52c0363670beadcf91d5272ea476eb1
Tinsae <itistinsae@gmail.com>
2022-03-29 23:59:48 +0300

    Update home.dart

--- lib/Presentation/screens/home.dart
+++ lib/Presentation/screens/home.dart
@@ -1,1 +1,69 @@
 
+import 'package:dev_player_fds/Data/models/models.dart';
+import 'package:dev_player_fds/Presentation/widgets/widgets.dart';
+
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: const CustomDrawer(),
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leadingWidth: 20,
+        // leading: BackButton(),
+
+        actions: const [
+          Padding(
+            padding: EdgeInsets.all(8.0),
+            child: NotificationButton(),
+          )
+        ],
+        title: CustomTitle('Football Drafting System'),
+        iconTheme: const IconThemeData(color: Colors.black),
+      ),
+
+      body: SafeArea(
+          child: Column(
+        children: [
+          SearchBar(),
+          Expanded(
+            child: ListView.builder(
+                scrollDirection: Axis.vertical,
+                padding: const EdgeInsets.all(20),
+                // itemCount: jobList.length,
+                itemCount: 5,
+                itemBuilder: (context, index) {
+                  return EventTile('', '');
+                }),
+          ),
+        ],
+      )),
+
+      // child: Column(
+      //   children: [
+      //     Expanded(
+      //       child: SizedBox(
+      //         height: 200,
+      //         child: ListView.builder(
+      //             scrollDirection: Axis.vertical,
+      //             padding: const EdgeInsets.all(20),
+      //             // itemCount: jobList.length,
+      //             itemCount: 5,
+      //             itemBuilder: (context, index) {
+      //               // return FadeAnimation(
+      //               //     (1.0 + index) / 4, jobComponent(job: jobList[index]));
+      //               return JobComponent('', '');
+      //             }),
+      //       ),
+      //     ),
+      //   ],
+      // ),
+    );
+  }
+}
