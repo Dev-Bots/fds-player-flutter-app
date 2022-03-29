0e541eb3d0582de7487a276a26488abd3fd7a453
Tinsae <itistinsae@gmail.com>
2022-03-29 19:38:35 +0300

    Update routes.dart

--- lib/Presentation/config/routes.dart
+++ lib/Presentation/config/routes.dart
@@ -1,1 +1,22 @@
 
+import 'package:dev_player_fds/Presentation/screens/screens.dart';
+import 'package:flutter/material.dart';
+
+class RouteGenerator {
+  static const String loginPage = "/login";
+  static const String homePage = "/"; //default page
+  static const String registerPage = "/register";
+
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case loginPage:
+        return MaterialPageRoute(builder: (_) => const LoginPage());
+      case homePage:
+        return MaterialPageRoute(builder: (_) => HomePage());
+      case registerPage:
+        return MaterialPageRoute(builder: (_) => const RegisterPage());
+      default:
+        throw const FormatException("Route was not found");
+    }
+  }
+}
