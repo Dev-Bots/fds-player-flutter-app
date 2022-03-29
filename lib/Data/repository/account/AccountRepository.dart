0e8b3de5e9c1f2b7e5d6c6a65d0b2a442c112e08
Tinsae <itistinsae@gmail.com>
2022-03-29 18:31:41 +0300

    Update AccountRepository.dart

--- lib/Data/repository/account/AccountRepository.dart
+++ lib/Data/repository/account/AccountRepository.dart
@@ -8,4 +8,18 @@ class AccountRepository {
 
   AccountRepository({required this.accountDataProvider});
 
-  Future getAllAccounts()
+  Future getAllAccounts() async {
+    return await accountDataProvider.getAccounts();
+  }
+  // Future getAllAccounts() async {
+  //   Account data = await accountDataProvider.getAccounts();
+  //   List accountJsonList = data.props; // data.props is a list of json objects
+  //   var accounts =
+  //       accountJsonList.map((account) => Account.fromJson(account)).toList();
+  //   return accounts;
+  // }
+
+  Future getCurrentUser() async {
+    return await accountDataProvider.getCurrentUser();
+  }
+}
