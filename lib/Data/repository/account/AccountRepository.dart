8fad56ce5e90a4ad41999bdaef97531bbd19beb0
Tinsae <itistinsae@gmail.com>
2022-03-29 18:25:12 +0300

    Update AccountRepository.dart

--- lib/Data/repository/account/AccountRepository.dart
+++ lib/Data/repository/account/AccountRepository.dart
@@ -1,1 +1,11 @@
 
+import 'package:dev_player_fds/Data/models/models.dart';
+import 'package:dev_player_fds/Data/provider/Account/AccountProvider.dart';
+import 'package:dev_player_fds/Data/provider/provider.dart';
+
+class AccountRepository {
+  final AccountDataProvider accountDataProvider;
+
+  AccountRepository({required this.accountDataProvider});
+
+  Future getAllAccounts()
