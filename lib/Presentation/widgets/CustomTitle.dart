b89fd722fe6e9bcf248ba2d57847b9445ec91716
Tinsae <itistinsae@gmail.com>
2022-03-31 00:42:23 +0300

    Update CustomTitle.dart

--- lib/Presentation/widgets/CustomTitle.dart
+++ lib/Presentation/widgets/CustomTitle.dart
@@ -1,1 +1,17 @@
+import 'package:flutter/material.dart';
+
+class CustomTitle extends StatelessWidget {
+  CustomTitle(this.title);
+  String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text("Football Drafting System",
+        style: TextStyle(
+          fontSize: 20,
+          fontWeight: FontWeight.w600,
+          color: Colors.black,
+        ));
+  }
+}
 
